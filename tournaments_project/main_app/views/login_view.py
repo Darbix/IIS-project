ac27a20da71c0918e08570387d40ad436c0e1830
Augustin Machynak <augustin.machynak@gmail.com>
2022-10-15 22:59:40 +0200

    Implementace registrace a login uživatele

--- tournaments_project/main_app/views/login_view.py
+++ tournaments_project/main_app/views/login_view.py
@@ -1,11 +1,31 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.http import HttpResponse
-from django.template import loader
-from django.urls import reverse
+from django.views.generic import TemplateView
+from django.contrib.auth.hashers import check_password
+from django.shortcuts import render
+
 from ..models import RegisteredUser
 
-def login_user(request):
-    template = loader.get_template('main_app/login_user.html')
-    args = {
-        "user": "ok"} # TODO test logged in user
-    return HttpResponse(template.render(args, request))
+class LoginUser(TemplateView):
+    index_template_name = 'main_app/login_user.html'
+    template_name = 'main_app/login_user.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+    
+    def post(self, request):
+        if "email" not in request.POST or "password" not in request.POST:
+            # TODO: Error hláška - chybějící email/heslo v post requestu
+            return render(request, self.index_template_name)
+        try:
+            user = RegisteredUser.objects.get(email=request.POST["email"])
+        except RegisteredUser.DoesNotExist as e:
+            # TODO: Error hláška - uživatel s tímto emailem neexistuje
+            return render(request, self.index_template_name)
+
+        passwordOk = check_password(request.POST["password"], user.password)
+        if not passwordOk:
+            # TODO: Error hláška - špatně zadané heslo
+            return render(request, self.index_template_name)
+        request.session['member_id'] = user.id
+        return render(request, self.index_template_name)
+
--- tournaments_project/main_app/views/main_view.py
+++ tournaments_project/main_app/views/main_view.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.http import HttpResponse
 from django.template import loader
 from django.urls import reverse
+
 from ..models import RegisteredUser
 
 # Create your views here.
@@ -11,17 +11,6 @@ def index(request):
         "users": users,
         "user": None} # TODO test logged out user
     return HttpResponse(loader.get_template('main_app/index.html').render(args, request))
-
-def add_user(request):
-    template = loader.get_template('main_app/add_user.html')
-    return HttpResponse(template.render({}, request))
-
-def add_user_post(request):
-    n = request.POST["name"]
-    user = RegisteredUser(first_name=n)
-    user.save()
-    return HttpResponseRedirect(reverse('index'))
-
 
 def delete_user(request):
     template = loader.get_template('main_app/delete_user.html')
